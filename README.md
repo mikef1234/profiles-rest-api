ce7cc644cfa5b0c9654bf0617c05de420fa6e263
mikef1234 <mikejferrari12@gmail.com>
2018-05-02 17:41:35 -0400

    All

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ django admin
 python manage.py runserver 0.0.0.0:8080
 go to 127.0.0.1:8080
 login into the email/password you entered in before
+
+APIView defines function uses standard http methods, GET, PosT, PUT, PATCH, Delete and the viewset. Gives you the most control over the logic. full control over logic. PRocessing files and rendering a synchronous response. Calling external API's. Access local files or data
+
+Viewset: 
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+# application logic behind API
+
+
+class HelloApiView(APIView):
+    '''Test API View'''
+
+    def get(self, request, format=None):
+        '''Return a list of APIView features'''
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put delete',
+            'Similar to a traditional Django view',
+            'Gives you the most control over your logic',
+            'is mapped manually to URLs'
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
