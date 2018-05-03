fb123440042d0096ca5b489f2f97bf81aff3c0b1
mikef1234 <mikejferrari12@gmail.com>
2018-05-03 09:33:09 -0400

    All

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,8 +3,10 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from . import serializers
 from . import models
+from . import permissions
 
 
 # Create your views here.
@@ -98,3 +100,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     '''handles creating, creating, and updating profiles'''
         serializer_class = serializers.UserProfileSerializer
         queryset = models.UserProfile.objects.all()
+        authentication_classes = (TokenAuthentication,)
+        permission_classes = (permissions.UpdateOwnProfile,)
