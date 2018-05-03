faeda557c12323d2bbabef6aeddf6e5b4a9e69c6
mikef1234 <mikejferrari12@gmail.com>
2018-05-03 11:42:36 -0400

    All

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -60,3 +60,14 @@ class UserProfile(AbstractBaseUser, PermissionMixin):
         the object to a string'''
 
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    '''Profile status update'''
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        '''Return the model as a string'''
+        return self.status_text
