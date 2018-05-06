0229d78750555dd6d27cd94d614a5b8138ddf693
mikef1234 <mikejferrari12@gmail.com>
2018-05-06 17:32:16 -0400

    TesT

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
 from . import serializers
 from . import models
 from . import permissions
@@ -101,3 +104,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class LoginViewSet(viewsets.ModelViewSet):
+    '''Checks email and password and returns an auth token'''
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        '''USe the ObtainAuthToken API View to validate and create a token'''
+        return ObtainAuthToken().post(request)
